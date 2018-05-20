9ef3fa9992fd95ec307860f88a59c7862204d4cd
John Martin <jrmartin182@gmail.com>
2018-05-20 13:20:47 -0600

    created grid

--- jscript.js
+++ jscript.js
@@ -1,13 +1,13 @@
 function divCreate(){
     var newDiv = document.createElement('div');
-    newDiv.className(gameCell)
+    newDiv.className= 'gameCell';
     newDiv.textContent = "testing 1 2 3";
-
-    var gameCell = document.getElementsByClassName('gameCell');
-    gameCell.appendChild(newDiv);
+  
+    var newContent =document.getElementById('gameCells');
+    newContent.appendChild(newDiv);
 }
 
-var gridSize = 5;
+var gridSize = 12;
 for (i = 0; i < gridSize; i++){
     divCreate();
 }
