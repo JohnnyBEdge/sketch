84ffdd451a20820b01299bdab916005cc6041a56
John Martin <jrmartin182@gmail.com>
2018-05-22 17:22:52 -0600

    added slider value; re-made div create

--- jscript.js
+++ jscript.js
@@ -1,14 +1,45 @@
-function divCreate(){
-    var newDiv = document.createElement('div');
-    newDiv.className= 'gameCell';
-    newDiv.textContent = "testing 1 2 3";
-  
-    var newContent =document.getElementById('gameCells');
-    newContent.appendChild(newDiv);
-}
+// function divCreate(){
+//     var newDiv = document.createElement('div');
+//     newDiv.className= 'gameCell';
+//     newDiv.id = 'cell';
+//     newDiv.textContent = "testing 1 2 3";
+//     //newDiv.setAttribute('onmouseenter', blackOut);
+     
+//     var newContent =document.getElementById('gameCells');
+//     newContent.appendChild(newDiv);
+// }
 
-var gridSize = 12;
-for (i = 0; i < gridSize; i++){
-    divCreate();
-}
+// var gridSize = 16;
+// for (i = 0; i < gridSize; i++){
+//     divCreate();
+// }
 
+// document.getElementsByClassName('.gameCell').onmouseover = function() {blackOut()};
+
+// function blackOut(){
+//     // var cell = document.getElementsByClassName('gameCell');
+//     // cell.style.backgroundColor = 'black'
+//     console.log('test')
+//}
+
+function divCreate(numDivs){
+    
+    for(i = 0; i < numDivs; i++){
+        for (j = 0; j < numDivs; j++){
+            var gridSquare = document.createElement('div');
+            gridSquare.className = 'gridSquare';
+            var apply = document.getElementById('gameCells');
+            apply.appendChild(gridSquare);
+        }
+      }
+    }
+divCreate(6);
+
+var slider = document.getElementById("rangeBar");
+var output = document.getElementById("gridSize");
+
+output.innerHTML = slider.value;
+
+slider.oninput = function() {
+  output.innerHTML = this.value;
+};
