7d002c748c1a37b2c472a6fa1ce5d8266843f094
John Martin <jrmartin182@gmail.com>
2018-06-01 16:58:21 -0600

    finished turn div to black function

--- jscript.js
+++ jscript.js
@@ -10,6 +10,7 @@ getContainer.style.gridTemplateColumns = 'repeat('+val+',1fr)';
 
 var createDiv = document.createElement('div');
 createDiv.className = "cell";
+createDiv.id= "cells";
 
 getContainer.appendChild(createDiv);
 }
@@ -19,6 +20,7 @@ var getContainer = document.getElementById('screenplay');
 
 var createRow = document.createElement('div');
 createRow.className = "cell";
+createRow.id = 'cells';
 
 getContainer.appendChild(createRow);
 }
@@ -33,11 +35,30 @@ for(var i = 0; i < val; i++){
 }
 
 createBoard(val);
- 
 
+//changes background to black, not yet working
 
-// //clears grid
-// function clearGrid(){
+//  let cellColor = 'black';
 
-//     location.reload()
-// }
+// var testing = document.getElementsByClassName("cell");
+// testing.addEventListener('mouseover', (e) => {
+//   e.target.style.backgroundColor = 'black';
+// });
+  //     for (var i = 0; i < test.length; i++){
+  //   test[i].style.backgroundColor = 'yellow';
+  // };
+
+  // testing.addEventListener('mouseover',
+  // function(e){
+  //     e.target.style.backgroundColor = 'black';
+  // });
+
+ //Turns grid color black
+    var colorBlack = document.getElementsByClassName("cell");
+    for (var i=0;i < colorBlack.length;i++){
+        (colorBlack[i].addEventListener('mouseover', function(e){
+          e.target.style.backgroundColor = 'black';
+        })
+      )}
+  
+
