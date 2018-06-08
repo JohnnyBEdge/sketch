28d696f5d1939280dd11f21b655db460eb0201af
John Martin <jrmartin182@gmail.com>
2018-06-08 09:52:24 -0600

    add black and color buttons and functions

--- jscript.js
+++ jscript.js
@@ -1,9 +1,10 @@
 function play(){
+var val = prompt("Enter grid size 2-100");
 
-var val = prompt("enter value");
-
-
+//creates divs
 function createDivs(){
+
+
 
 var getContainer = document.getElementById('screenplay');
 getContainer.style.display= 'grid';
@@ -26,8 +27,9 @@ createRow.id = 'cells';
 getContainer.appendChild(createRow);
 }
 
+//creates grid
 function createBoard(){
- for(var i = 0; i < val; i++){
+  for(var i = 0; i < val; i++){
   createDivs();
   for(var j = 0; j < val-1; j++){
   createDivRow() 
@@ -36,12 +38,44 @@ function createBoard(){
 }
 
 createBoard();
+};
+ //Turns grid multicolored
+ function colorize(){
+    var colorBlack = document.getElementsByClassName("cell");
+    for (var i=0;i < colorBlack.length;i++){
+        (colorBlack[i].addEventListener('mouseover', function(e){
+          e.target.style.backgroundColor = addRandomBackgroundColor(e);
+        })
+      )}
+    }
+    
+ 
 
- //Turns grid color black
+
+    //Turns grid color black
+function blackout(){
     var colorBlack = document.getElementsByClassName("cell");
     for (var i=0;i < colorBlack.length;i++){
         (colorBlack[i].addEventListener('mouseover', function(e){
           e.target.style.backgroundColor = 'black';
         })
       )}
+    };
+
+    //creates random color
+
+    function addRandomBackgroundColor(e) {
+      e.target.style.backgroundColor = randomColor();
     }
+    
+    function randomColor() {
+      return `rgb(${randomNum(256)}, ${randomNum(256)}, ${randomNum(256)})`;
+    }
+    
+    function randomNum(max) {
+      return Math.floor(Math.random() * Math.floor(max)); //The maximum is exclusive
+    };
+
+    function refresh(){
+      location.reload();
+    };
